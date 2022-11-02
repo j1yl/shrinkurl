2133777e577bfd48047f089e42a47e1fe8e6c527
Joe Lee <lefodev@gmail.com>
2022-11-01 21:31:57 -0700

    Add boilerplate html

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,23 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <section>
+        <div>FORGET LONG LINKS</div>
+        <div>LET&apos;S MAKE URLS EASY</div>
+        <form>
+          <input placeholder="HTTPS://YOURURLHERE.COM" />
+          <button>SHRINK</button>
+        </form>
+      </section>
+      <section>
+        <div>SHRUNKEN LINK</div>
+        <form>
+          <div>HTTPS://SHRUNKENURL.COM</div>
+          <button>COPY</button>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Hero;
