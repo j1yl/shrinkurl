4ace0515b4c0170ff3ff1dc5593a76f35f615d29
Joe Lee <lefodev@gmail.com>
2022-11-01 22:25:22 -0700

    Add about section html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
     <>
-      <div className="App h-screen w-screen bg-black text-white mx-auto font-mont">
-        <header className="max-w-4xl mx-auto">
+      <div className="App w-screen bg-black text-white font-mont">
+        <header className="max-w-4xl mx-auto w-full">
           <Navbar />
         </header>
-        <main className="max-w-4xl mx-auto h-[90vh]">
+        <main className="max-w-4xl mx-auto h-[90vh] w-full">
           <Hero />
         </main>
+        <section className="h-screen w-full max-w-4xl mx-auto flex justify-center flex-col gap-5">
+          <About />
+        </section>
       </div>
     </>
   );
---
+++ src/components/About.tsx
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <>
+      <h2 className="text-6xl">ABOUT</h2>
+      <p className="text-3xl">
+        SHRINKURL IS A WEB APPLICATION WITH AN INTERACTIVE MINIMALIST INTERFACE
+        THAT UTILIZES THE GOTINY API TO SHORTEN LINKS. THIS PROJECT IS MADE WITH
+        TYPESCRIPT, REACT, FRAMER, AND TAILWINDCSS. THE ENTIRE SOURCE CODE CAN
+        BE FOUND ON THE GITHUB REPOSITORY.
+      </p>
+      <a
+        href="https://github.com/j1yl/shrinkurl"
+        className="text-3xl underline"
+      >
+        VIEW CODE
+      </a>
+    </>
+  );
+};
+
+export default About;
