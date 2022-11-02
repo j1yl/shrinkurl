11ac0bd7efe5e01d2d114510499184fbb88c85e0
Joe Lee <lefodev@gmail.com>
2022-11-01 21:29:41 -0700

    Remove redundancies in changing font

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero";
 function App() {
   return (
     <>
-      <div className="App h-screen w-screen bg-black text-white mx-auto font-mono">
+      <div className="App h-screen w-screen bg-black text-white mx-auto font-mont">
         <header className="max-w-4xl mx-auto">
           <Navbar />
         </header>
