bf8c196033045f8ac34e5b37a03fd7a71a178058
Joe Lee <lefodev@gmail.com>
2022-11-01 21:25:18 -0700

    Restructured the html

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App h-screen w-screen bg-black text-white mx-auto font-mono">
-      <header className="max-w-4xl mx-auto">
-        <Navbar />
-      </header>
-    </div>
+    <>
+      <div className="App h-screen w-screen bg-black text-white mx-auto font-mono">
+        <header className="max-w-4xl mx-auto">
+          <Navbar />
+        </header>
+        <main className="max-w-4xl mx-auto"></main>
+      </div>
+    </>
   );
 }
 
